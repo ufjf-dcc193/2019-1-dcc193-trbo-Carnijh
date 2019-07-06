b7c0651f69c95ccaf7174f292db4942cb40bdf0e
Caroliveira <caroliveira.ti@gmail.com>
2019-07-06 12:58:35 -0300

    Initial Commit

---
+++ src/main/java/br/ufjf/dcc193/trbo/MainApplication.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.trbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
