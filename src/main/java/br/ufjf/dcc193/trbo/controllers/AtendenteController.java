d8bf40b28278b74e30b3e7bb9daa5eeec22fb430
Carnijh <carnijh@hotmail.com>
2019-07-07 12:53:17 -0300

    Controller do atendente criado

---
+++ src/main/java/br/ufjf/dcc193/trbo/controllers/AtendenteController.java
@@ -0,0 +1,10 @@
+package br.ufjf.dcc193.trbo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * AtendenteController
+ */
+@Controller
+public class AtendenteController {
+}
