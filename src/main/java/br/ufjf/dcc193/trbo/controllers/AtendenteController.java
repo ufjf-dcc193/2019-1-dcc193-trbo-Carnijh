ab981f21030147ce6fcc5d4594856019cb7c59af
Carnijh <carnijh@hotmail.com>
2019-07-07 13:01:26 -0300

    template criar atendente criado

--- src/main/java/br/ufjf/dcc193/trbo/controllers/AtendenteController.java
+++ src/main/java/br/ufjf/dcc193/trbo/controllers/AtendenteController.java
@@ -1,10 +1,16 @@
 package br.ufjf.dcc193.trbo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * AtendenteController
  */
 @Controller
 public class AtendenteController {
+    // CHAMA TELA CRIAR ATENDENTE
+    @RequestMapping("/atendente/criar.html")
+    public String criar() {
+        return "/atendente/criar.html";
+    }
 }
