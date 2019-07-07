91262830044dd6ad088bfb37b71e99222e6c47b3
Carnijh <carnijh@hotmail.com>
2019-07-07 11:30:36 -0300

    Repositorio de Atendente criado

---
+++ src/main/java/br/ufjf/dcc193/trbo/repositorys/AtendenteRepository.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc193.trbo.repositorys;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufjf.dcc193.trbo.models.Atendente;
+
+/**
+ * AtendenteRepository
+ */
+public interface AtendenteRepository extends JpaRepository<Atendente, Long> {
+    List<Atendente> findAll();
+    void deleteById(Long id);
+}
