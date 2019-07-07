24a8a921ae66e1d5195f66b3c4e7888c89328ea8
Carnijh <carnijh@hotmail.com>
2019-07-07 11:15:38 -0300

    Classe Categoria Criada

---
+++ src/main/java/br/ufjf/dcc193/trbo/models/Categoria.java
@@ -0,0 +1,37 @@
+package br.ufjf.dcc193.trbo.models;
+
+/**
+ * Categoria
+ */
+public class Categoria {
+    // título e descrição textual.
+    private Long id;
+    private String titulo;
+    private String descricao;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return this.titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+    
+}
